6710488b1b727e439ef8ba53e307fa1e1bafe504
revans <richard.evans@xtxmarkets.com>
2015-12-04 10:21:18 +0000

    Cleanup

--- advent4.py
+++ advent4.py
@@ -1,10 +1,6 @@
-import copy
-import hashlib
-import os
-import requests
-
+import copy, hashlib, os, requests
 key = requests.get('http://adventofcode.com/day/4/input',
-                          cookies=dict(session=os.environ['ADVENT_SESSION'])).content.strip()
+                   cookies=dict(session=os.environ['ADVENT_SESSION'])).content.strip()
 
 code = 0
 zeros = {5, 6}
@@ -14,7 +10,7 @@ while not not zeros:
     code += 1
     md5hash = hashlib.md5(key + str(code)).hexdigest()
     for val in copy.copy(zeros):
-        if md5hash[:val] == ('0'*val):
+        if md5hash[:val] == '0'*val:
             answers[val] = code
             zeros.remove(val)
 
