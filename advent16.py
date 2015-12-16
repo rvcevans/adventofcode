45ad5403909a25ba75f64f482f116a419631ee82
revans <richard.evans@xtxmarkets.com>
2015-12-16 11:01:05 +0000

    Small cleanup to 16

--- advent16.py
+++ advent16.py
@@ -1,29 +1,29 @@
-import operator, os, re, requests, itertools
+import os, re, requests, itertools
+from operator import eq, gt, lt
 descriptions = requests.get('http://adventofcode.com/day/16/input',
                             cookies=dict(session=os.environ['ADVENT_SESSION'])).content.strip()
 
 
 machine = {'children': 3, 'cats': 7, 'samoyeds': 2, 'pomeranians': 3, 'akitas': 0,
            'vizslas': 0, 'goldfish': 5, 'trees': 3, 'cars': 2, 'perfumes': 1}
-operators = {'children': operator.eq, 'cats': operator.gt, 'samoyeds': operator.eq, 'pomeranians': operator.lt,
-             'akitas': operator.eq, 'vizslas': operator.eq, 'goldfish': operator.lt, 'trees': operator.gt,
-             'cars': operator.eq, 'perfumes': operator.eq}
+operators = {'children': eq, 'cats': gt, 'samoyeds': eq, 'pomeranians': lt, 'akitas': eq,
+             'vizslas': eq, 'goldfish': lt, 'trees': gt, 'cars': eq, 'perfumes': eq}
 
 aunts = {}
 for d in descriptions.splitlines():
-    i = re.match('Sue (\d+): (\w+): (\d+), (\w+): (\d+), (\w+): (\d+)', d)
-    aunts[int(i.group(1))] = {i.group(2 * j + 2): int(i.group(2 * j + 3)) for j in xrange(3)}
+    rm = re.match('Sue (\d+): (\w+): (\d+), (\w+): (\d+), (\w+): (\d+)', d)
+    aunts[int(rm.group(1))] = {rm.group(2 * j): int(rm.group(2 * j + 1)) for j in xrange(1, 4)}
 
 for number, aunt in aunts.iteritems():
-    for at, value in aunt.iteritems():
-        if machine[at] != value:
+    for com, value in aunt.iteritems():
+        if machine[com] != value:
             break
 
     else:
         print 'Fake Sue: %s' % number
 
-    for at, value in aunt.iteritems():
-        if not operators[at](value, machine[at]):
+    for com, value in aunt.iteritems():
+        if not operators[com](value, machine[com]):
             break
 
     else:
