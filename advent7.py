34ffe9012f348f96f8142356d6af920d27888bd5
revans <richard.evans@xtxmarkets.com>
2015-12-29 10:05:15 +0000

    tidy up

--- advent7.py
+++ advent7.py
@@ -1,18 +1,21 @@
 import os, re, requests
 from operator import and_, or_, rshift, lshift
-instructions = requests.get('http://adventofcode.com/day/7/input',
+puzzle_input = requests.get('http://adventofcode.com/day/7/input',
                             cookies=dict(session=os.environ['ADVENT_SESSION'])).content.strip()
+
+
+def instructions(network):
+    for d in network.splitlines():
+        yield re.match('(?:(\w{1,2}|\d+) )?(AND|OR|LSHIFT|RSHIFT|NOT)? ?([\w]+) -> (\w+)', d).group(1, 2, 3, 4)
 
 
 def get(node, nodes):
     return int(node) if node.isdigit() else nodes[node]
 
 
-def get_nodes(instructions):
+def get_nodes(network):
     nodes = {}
-    for d in instructions.splitlines():
-        rm = re.match('(?:(\w{1,2}|\d+) )?(AND|OR|LSHIFT|RSHIFT|NOT)? ?([\w]+) -> (\w+)', d)
-        (ina, inb, out) = rm.group(1, 3, 4)
+    for (ina, op, inb, out) in instructions(network):
         nodes[out] = None
         if not inb.isdigit():
             nodes[inb] = None
@@ -24,44 +27,32 @@ def get_nodes(instructions):
     return nodes
 
 
-def evaluate(instructions, nodes, overides=None):
-    if overides is not None:
-        for o in overides:
-            nodes[o] = overides[o]
+def evaluate(network, nodes, overrides=()):
+    for o in overrides:
+        nodes[o] = overrides[o]
 
-    for d in instructions.splitlines():
-        rm = re.match('(?:(\w{1,2}|\d+) )?(AND|OR|LSHIFT|RSHIFT|NOT)? ?([\w]+) -> (\w+)', d)
-        (ina, op, inb, out) = rm.group(1, 2, 3, 4)
-        # if the out node has been evaluated then continue to the next instruction
+    for (ina, op, inb, out) in instructions(network):
         if not nodes[out] is None:
             continue
 
-        bvalue = get(inb, nodes)
-        if bvalue is None:
+        b = get(inb, nodes)
+        if b is None:
             continue
 
         if ina is None:
-            if op == 'NOT':
-                nodes[out] = ~ bvalue % 65536
-
-            else:
-                nodes[out] = bvalue
+            nodes[out] = ~ b % 65536 if op == 'NOT' else b
 
         else:
-            avalue = get(ina, nodes)
-            if avalue is None:
+            a = get(ina, nodes)
+            if a is None:
                 continue
 
-            nodes[out] = {'AND': and_, 'OR': or_, 'LSHIFT': lshift, 'RSHIFT': rshift}[op](avalue, bvalue)
+            nodes[out] = {'AND': and_, 'OR': or_, 'LSHIFT': lshift, 'RSHIFT': rshift}[op](a, b)
 
-    # print nodes
-    if None in nodes.values():
-        return evaluate(instructions, nodes)
-
-    return nodes
+    return evaluate(network, nodes) if None in nodes.values() else nodes
 
 
-networks = {1: evaluate(instructions, get_nodes(instructions))}
-networks[2] = evaluate(instructions, get_nodes(instructions), {'b': networks[1]['a']})
+networks = {1: evaluate(puzzle_input, get_nodes(puzzle_input))}
+networks[2] = evaluate(puzzle_input, get_nodes(puzzle_input), {'b': networks[1]['a']})
 
 print('\n'.join('Part {} value of wire a: {}'.format(key, val['a']) for key, val in networks.items()))
