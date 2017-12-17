1b978002b8027eda09b7bafd152591e215ba1ad7
Molly Taylor <Molly@mollys-mbp.home>
2017-12-17 13:46:25 +0000

    2015-15

---
+++ 2017/15.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"github.com/rvcevans/adventofcode/getinput"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	mulA = 16807
+	mulB = 48271
+	div  = 1<<31 - 1
+)
+
+func main() {
+	input := getinput.MustGet(2017, 15, os.Getenv("ADVENT_SESSION"))
+	a, b := generator(input[0]), generator(input[1])
+
+	achan, bchan := make(chan int, 50000000), make(chan int, 50000000)
+	pairs := 0
+	total1, total2 := 0, 0
+	go func() {
+		for {
+			if bitEqual(<-achan, <-bchan) {
+				total2++
+			}
+			pairs++
+		}
+	}()
+
+	i := 0
+	for pairs < 5000000 {
+		i ++
+		a = a * mulA % div
+		b = b * mulB % div
+		if i < 40000000 && bitEqual(a, b) {
+			total1++
+		}
+		if a%4 == 0 {
+			select {
+			case achan <- a:
+			default:
+			}
+		}
+		if b%8 == 0 {
+			select {
+			case bchan <- b:
+			default:
+			}
+		}
+	}
+
+	fmt.Println(total1)
+	fmt.Println(total2)
+}
+
+func generator(row string) int {
+	fields := strings.Fields(row)
+	i, err := strconv.Atoi(fields[len(fields)-1])
+	if err != nil {
+		log.Fatalf("Failed to parse %v to int: %v", i, err)
+	}
+	return i
+}
+
+func bitEqual(a, b int) bool {
+	return a&0xFFFF == b&0xFFFF
+}
